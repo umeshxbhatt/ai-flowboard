85d4bc9184b04c8f69cee0b8361446cc82347815
Umesh <ub.umeshbhatt552@gmail.com>
2026-09-05 22:08:20 +0530

    enhance comments in jwt.js for improved clarity and understanding

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -1,9 +1,16 @@
 import jwt from "jsonwebtoken";
 
+// Create token
 export const signToken = (payload) => {
+  // payload is the data we want to put inside the token
+
   return jwt.sign(payload, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN || "7d",
   });
+
+  // jwt.sign(payload, secret, options)
+  // Create a JWT using this payload, sign it with my secret, and make it expire after the configured amount of time (or 7 days by default).
 };
 
+// Checks token
 export const verifyToken = (token) => jwt.verify(token, process.env.JWT_SECRET);
