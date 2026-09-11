cc213c5c8b158414b0c5df0ed239c7533a5773a8
Umesh <ub.umeshbhatt552@gmail.com>
2026-09-11 23:18:18 +0530

    fix asyncHandler export to default for proper module usage

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,6 +1,7 @@
-export const asyncHandler = (fn) => (req, res, next) =>
+const asyncHandler = (fn) => (req, res, next) =>
   Promise.resolve(fn(req, res, next)).catch(next);
 
+export default asyncHandler;
 /* 
 
 the above code is shorthand for the following code:
